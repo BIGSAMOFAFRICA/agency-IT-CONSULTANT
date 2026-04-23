bae948204f795dc296596701aa5ceec6eba60656
BIGSAMOFAFRICA <olabisisamuelayomide@gmail.com>
2026-04-23 15:33:21 +0100

    logo-added

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { Link, useLocation } from 'react-router-dom'
 import { Menu, X } from 'lucide-react'
-import Logo from '../assets/logo.png'
+import Logo from '../assets/logo.jpg'
 
 const navItems = [
   { to: '/', label: 'Home' },
